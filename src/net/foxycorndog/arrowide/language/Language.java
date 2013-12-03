2ad2fc56af7774789498f437bf80d901a14b5195
Braden Steffaniak <BradenSteffaniak@gmail.com>
2013-12-03 06:07:21 -0600

    Click for details Made Language.run(...) not a static method. Auto organizes the keywords into an array.

--- src/net/foxycorndog/arrowide/language/Language.java
+++ src/net/foxycorndog/arrowide/language/Language.java
@@ -17,7 +17,7 @@ import net.foxycorndog.arrowide.console.ConsoleStream;
 import net.foxycorndog.arrowide.event.CompilerListener;
 import net.foxycorndog.arrowide.event.ProgramListener;
 import net.foxycorndog.arrowide.file.FileUtils;
-import net.foxycorndog.arrowide.xml.Reader;
+import net.foxycorndog.arrowide.xml.XMLReader;
 import net.foxycorndog.arrowide.xml.XMLItem;
 import static net.foxycorndog.arrowide.ArrowIDE.PROPERTIES;
 
@@ -25,15 +25,13 @@ public class Language
 {
 	private Image						image;
 	
+	private String						keywords[];
+	
 	private HashMap<String, XMLItem[]>	properties;
 	
 //	public  static final int JAVA = FileUtils.JAVA, GLSL = FileUtils.GLSL, ASSEMBLY = FileUtils.ASSEMBLY, FOXY = FileUtils.FOXY, CPP = FileUtils.CPP, C = FileUtils.C, PHP = FileUtils.PHP, PYTHON = FileUtils.PYTHON;
 	
 	private static ArrayList<CompilerListener>				listeners;
-	
-	private static HashMap<Integer, CommentProperties>		commentProperties;
-	private static HashMap<Integer, MethodProperties>		methodProperties;
-	private static HashMap<Integer, IdentifierProperties>	identifierProperties;
 	
 	private static HashMap<String, Language>				languages;
 	
@@ -41,56 +39,7 @@ public class Language
 	{
 		listeners = new ArrayList<CompilerListener>();
 		
-		commentProperties    = new HashMap<Integer, CommentProperties>();
-		methodProperties     = new HashMap<Integer, MethodProperties>();
-		identifierProperties = new HashMap<Integer, IdentifierProperties>();
-		
 		languages            = new HashMap<String, Language>();
-		
-//		Keyword.addLanguage(JAVA);
-//		Keyword.addLanguage(GLSL);
-//		Keyword.addLanguage(ASSEMBLY);
-//		Keyword.addLanguage(FOXY);
-//		Keyword.addLanguage(CPP);
-//		Keyword.addLanguage(C);
-//		Keyword.addLanguage(PHP);
-//		Keyword.addLanguage(PYTHON);
-//		
-//		JavaLanguage.init();
-//		GLSLLanguage.init();
-//		AssemblyLanguage.init();
-//		FoxyLanguage.init();
-//		CppLanguage.init();
-//		CLanguage.init();
-//		PHPLanguage.init();
-//		PythonLanguage.init();
-//		
-//		commentProperties.put(JAVA, JavaLanguage.COMMENT_PROPERTIES);
-//		commentProperties.put(GLSL, GLSLLanguage.COMMENT_PROPERTIES);
-//		commentProperties.put(ASSEMBLY, AssemblyLanguage.COMMENT_PROPERTIES);
-//		commentProperties.put(FOXY, FoxyLanguage.COMMENT_PROPERTIES);
-//		commentProperties.put(CPP, CppLanguage.COMMENT_PROPERTIES);
-//		commentProperties.put(C, CppLanguage.COMMENT_PROPERTIES);
-//		commentProperties.put(PHP, PHPLanguage.COMMENT_PROPERTIES);
-//		commentProperties.put(PYTHON, PythonLanguage.COMMENT_PROPERTIES);
-//		
-//		methodProperties.put(JAVA, JavaLanguage.METHOD_PROPERTIES);
-//		methodProperties.put(GLSL, GLSLLanguage.METHOD_PROPERTIES);
-//		methodProperties.put(ASSEMBLY, AssemblyLanguage.METHOD_PROPERTIES);
-//		methodProperties.put(FOXY, FoxyLanguage.METHOD_PROPERTIES);
-//		methodProperties.put(CPP, CppLanguage.METHOD_PROPERTIES);
-//		methodProperties.put(C, CppLanguage.METHOD_PROPERTIES);
-//		methodProperties.put(PHP, PHPLanguage.METHOD_PROPERTIES);
-//		methodProperties.put(PYTHON, PythonLanguage.METHOD_PROPERTIES);
-//		
-//		identifierProperties.put(JAVA, JavaLanguage.IDENTIFIER_PROPERTIES);
-//		identifierProperties.put(GLSL, GLSLLanguage.IDENTIFIER_PROPERTIES);
-//		identifierProperties.put(ASSEMBLY, AssemblyLanguage.IDENTIFIER_PROPERTIES);
-//		identifierProperties.put(FOXY, FoxyLanguage.IDENTIFIER_PROPERTIES);
-//		identifierProperties.put(CPP, CppLanguage.IDENTIFIER_PROPERTIES);
-//		identifierProperties.put(C, CppLanguage.IDENTIFIER_PROPERTIES);
-//		identifierProperties.put(PHP, PHPLanguage.IDENTIFIER_PROPERTIES);
-//		identifierProperties.put(PYTHON, PythonLanguage.IDENTIFIER_PROPERTIES);
 		
 		File root = new File((String)PROPERTIES.get("resources.location") + "res/languages");
 		
@@ -122,7 +71,38 @@ public class Language
 	
 	public Language(String xmlLocation)
 	{
-		properties = Reader.read(xmlLocation);
+		properties = XMLReader.read(xmlLocation);
+		
+		keywords = new String[0];
+		
+		XMLItem items[] = getItems("language.keywords");
+
+		for (int i = 0; i < items.length; i++)
+		{
+			String contents = items[i].getContents();
+			
+			contents = contents.replace("\n", "");
+			contents = contents.replace("\r", "");
+			contents = contents.replace("\t", "");
+			contents = contents.replace("  ", "");
+			contents = contents.replace("   ", "");
+			
+			String list[] = contents.split(" ");
+			
+			if (keywords.length == 0)
+			{
+				keywords = list;
+			}
+			else
+			{
+				String newKeywords[] = new String[keywords.length + list.length];
+
+				System.arraycopy(keywords, 0, newKeywords, 0, keywords.length);
+				System.arraycopy(list, 0, newKeywords, keywords.length, list.length);
+				
+				keywords = newKeywords;
+			}
+		}
 		
 		String imageName = getAttribute("language>image");
 		
@@ -149,16 +129,16 @@ public class Language
 		return Boolean.valueOf(getAttribute("language>executable"));
 	}
 	
-	public static Program run(String fileLocation, ConsoleStream stream)
+	public Program run(String fileLocation, ConsoleStream stream)
 	{
 		return run(fileLocation, stream, null);
 	}
 	
-	public static Program run(String fileLocation, ConsoleStream stream, ProgramListener listener)
+	public Program run(String fileLocation, ConsoleStream stream, ProgramListener listener)
 	{
 		fileLocation = FileUtils.removeEndingSlashes(fileLocation.replace('\\', '/'));
 		
-		if (true)
+		if (!canRun())
 		{
 			throw new UnsupportedOperationException("The execution of the specified language is not currently supported by ArrowIDE.");
 		}
@@ -181,52 +161,11 @@ public class Language
 		return "Arrow IDE does not support compiling the specified file type.";
 	}
 	
-	public static CommentProperties getCommentProperties(int language)
-	{
-		CommentProperties properties = null;
-		
-		if (commentProperties.containsKey(language))
-		{
-			properties = commentProperties.get(language);
-		}
-		
-		return properties;
-	}
-	
-	public static MethodProperties getMethodProperties(int language)
-	{
-		MethodProperties properties = null;
-		
-		if (methodProperties.containsKey(language))
-		{
-			properties = methodProperties.get(language);
-		}
-		
-		return properties;
-	}
-	
-	public static IdentifierProperties getIdentifierProperties(int language)
-	{
-		IdentifierProperties properties = null;
-		
-		if (identifierProperties.containsKey(language))
-		{
-			properties = identifierProperties.get(language);
-		}
-		
-		return properties;
-	}
-	
 	public void compile(String fileLocation, String code, String outputLocation, PrintStream stream)
 	{
-		if (true)
+		if (!canCompile())
 		{
 			throw new UnsupportedOperationException(getCompileError());
-		}
-		
-		if (fileLocation != null)
-		{
-			
 		}
 	}
 	
@@ -238,17 +177,63 @@ public class Language
 	public String getAttribute(String param)
 	{
 		String params[] = param.split(">");
+
+		XMLItem items[] = properties.get(params[0]);
 		
-		return properties.get(params[0])[0].getAttributes().get(params[1]);
+		if (items != null)
+		{
+			XMLItem item = items[0];
+			
+			if (item != null)
+			{
+				return item.getAttributes().get(params[1]);
+			}
+		}
+		
+		return null;
+	}
+	
+	public String[] getKeywords()
+	{
+		return keywords;
 	}
 	
 	public String getContents(String param)
 	{
 		String params[] = param.split(">");
 		
-		int index = Integer.valueOf(params[1]);
+		int index = 0;
 		
-		return properties.get(params[0])[index].getContents();
+		if (params.length > 1)
+		{
+			Integer.valueOf(params[1]);
+		}
+		
+		XMLItem items[] = properties.get(params[0]);
+		
+		if (items != null)
+		{
+			XMLItem item = items[index];
+			
+			if (item != null)
+			{
+				return item.getContents();
+			}
+		}
+		
+		return null;
+	}
+	
+	public XMLItem[] getItems(String param)
+	{
+		XMLItem items[] = properties.get(param);
+		
+		if (items == null)
+		{
+			return new XMLItem[0];
+		}
+		
+		return items;
 	}
 	
 	public static Language getLanguageByExtension(String location)
